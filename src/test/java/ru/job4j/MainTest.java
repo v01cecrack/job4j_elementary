0a464234c7046b08ac8197da1d3a132e15e2a63c
Kostya <k.kharionovsky@cloudcom.ru>
2024-06-22 15:41:11 +0300

    Фиксация изменения файлови на github [#243564]

---
+++ src/test/java/ru/job4j/MainTest.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+    @Test
+    public void whenActionThenResult() {
+        int excepted = 1;
+        int result = 1;
+        assertThat(result).isEqualTo(excepted);
+    }
+}
